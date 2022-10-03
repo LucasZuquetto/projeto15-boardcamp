64200d19f0493c3a7bd352044de504beaa9ebf86
LucasZuq <lucasdeozuquetto@hotmail.com>
2022-10-03 12:22:10 -0300

    validate categorie's name

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,20 +1,27 @@
-import { connection } from "../database/db.js"
+import { connection } from "../database/db.js";
 
-async function getCategories(req,res){
-    try {
-        const categories = (await connection.query('SELECT * FROM categories')).rows
-        res.send(categories)
-    } catch (error) {
-        console.log(error.message)
-        res.sendStatus(500)
-    }
-} 
-
-async function postCategories (req,res){
-    return
+async function getCategories(req, res) {
+   try {
+      const categories = (await connection.query("SELECT * FROM categories"))
+         .rows;
+      res.send(categories);
+   } catch (error) {
+      console.log(error.message);
+      res.sendStatus(500);
+   }
 }
 
-export {
-    postCategories,
-    getCategories
+async function postCategories(req, res) {
+   const { name } = req.body;
+   try {
+      await connection.query("INSERT INTO categories (name) VALUES ($1)", [
+         name,
+      ]);
+      res.sendStatus(201);
+   } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+   }
 }
+
+export { postCategories, getCategories };
--- src/middlewares/categoriesMiddleware.js
+++ src/middlewares/categoriesMiddleware.js
@@ -1,3 +1,17 @@
+import { connection } from "../database/db.js"
+
 export default async function categoriesMiddleware (req,res,next){
-    return
+    const {name} = req.body
+
+    if(!name){
+        res.sendStatus(400)
+        return
+    }
+
+    const categorieExists = await connection.query('SELECT * FROM categories WHERE name=$1;',[name])
+    if(categorieExists){
+        res.sendStatus(409)
+        return
+    }
+    next()
 }
